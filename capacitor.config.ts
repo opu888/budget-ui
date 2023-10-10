d87ff455f8e87ab77b666720c6fb48815324c87b
opu888 <63110428+opu888@users.noreply.github.com>
2023-10-10 19:21:02 +0200

    Reformat Code mit Prettier

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,7 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'Budget UI',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
 };
 
 export default config;
