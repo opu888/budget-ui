dd6a369fea5e266c7526547bdfd47b8c6907f980
opu888 <63110428+opu888@users.noreply.github.com>
2023-12-02 22:44:21 +0100

    Ausgabe bearbeiten & löschen Model weiter verschossen. Date und Kategorien werden nicht mehr angezeigt. Löschen Button auch nicht. Gebe auf für heute. Sehe es nicht mehr.

--- src/app/expense/expense-modal/expense-modal.component.ts
+++ src/app/expense/expense-modal/expense-modal.component.ts
@@ -1,6 +1,6 @@
-import {Component, Output} from '@angular/core';
+import {Component, OnInit, Output} from '@angular/core';
 import {ModalController, RefresherCustomEvent} from '@ionic/angular';
-import {filter, finalize, from, pipe} from 'rxjs';
+import {filter, finalize, from, mergeMap, pipe, tap} from 'rxjs';
 import { CategoryModalComponent } from '../../category/category-modal/category-modal.component';
 import { ActionSheetService } from '../../shared/service/action-sheet.service';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
@@ -26,6 +26,7 @@ export class ExpenseModalComponent {
   datePipe = new DatePipe('en-US');
 
 
+
   constructor(
     private readonly actionSheetService: ActionSheetService,
     private readonly modalCtrl: ModalController,
@@ -34,13 +35,13 @@ export class ExpenseModalComponent {
     private readonly categoryService: CategoryService,
     private readonly expenseService: ExpenseService,
     private modalController: ModalController,
-  ) {
-    this.expenseForm = this.formBuilder.group({
+  ) {this.expenseForm = this.formBuilder.group({
       id: [],
       name: ['', [Validators.required, Validators.maxLength(40)]], categoryId: [this.expense.categoryId],
       date: [this.datePipe.transform(Date.now(), 'yyyy-MM-ddThh:mm:ss')],
       amount: [''],
     })
+
   }
 
     async ngOnInit(): Promise<void> {
@@ -64,6 +65,7 @@ export class ExpenseModalComponent {
 
     save(): void {
         this.submitting = true;
+        let exp = this.expenseForm.value as Expense;
         this.expenseService
             .upsertExpense(this.expenseForm.value)
             .pipe(finalize(() => (this.submitting = false)))
@@ -78,14 +80,23 @@ export class ExpenseModalComponent {
 
   delete(): void {
     from(this.actionSheetService.showDeletionConfirmation('Are you sure you want to delete this expense?'))
-      .pipe(filter((action) => action === 'delete'))
-      .subscribe(() => this.modalCtrl.dismiss(null, 'delete'));
+        .pipe(
+            filter((action) => action === 'delete'),
+            tap(() => (this.submitting = true)),
+            mergeMap(() => this.expenseService.deleteExpense(this.expense.id!)),
+            finalize(() => (this.submitting = false)),
+        )
+        .subscribe({
+            next: () => {
+                this.toastService.displaySuccessToast('Expense deleted');
+                this.modalCtrl.dismiss(null, 'refresh');
+            },
+            error: (error) => this.toastService.displayErrorToast('Could not delete expense', error),
+        });
   }
 
     ionViewWillEnter(): void {
         this.expenseForm.patchValue(this.expense);
-
-
     }
 
 
