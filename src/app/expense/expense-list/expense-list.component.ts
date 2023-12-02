2011a88f89a198d36c9e5c7f0325131060b7f51e
opu888 <63110428+opu888@users.noreply.github.com>
2023-12-02 18:32:59 +0100

    expense-modal.component.html angepasst Ausgaben anzeigen angefangen

--- src/app/expense/expense-list/expense-list.component.ts
+++ src/app/expense/expense-list/expense-list.component.ts
@@ -3,6 +3,11 @@ import { addMonths, set } from 'date-fns';
 import { ModalController } from '@ionic/angular';
 import { ExpenseModalComponent } from '../expense-modal/expense-modal.component';
 import { Expense } from '../../shared/domain';
+import {DatePipe} from "@angular/common";
+import {CategoryService} from "../../category/category.service";
+import {ExpenseService} from "../expense.service";
+import {ToastService} from "../../shared/service/toast.service";
+import {FormBuilder} from "@angular/forms";
 
 @Component({
   selector: 'app-expense-overview',
@@ -10,12 +15,25 @@ import { Expense } from '../../shared/domain';
 })
 export class ExpenseListComponent {
   date = set(new Date(), { date: 1 });
+  currentMonth: Date = new Date();
+  datePipe: DatePipe = new DatePipe('en-US');
+
+  expenses: Expense[] = [];
+  lastPageReached = false;
+  loading = false;
+  readonly  initialSort = 'date,desc';
 
   changeMonth(monthDelta: number): void {
     this.date = addMonths(this.date, monthDelta);
   }
 
-  constructor(private readonly modalCtrl: ModalController) {}
+  constructor(
+    private readonly modalCtrl: ModalController,
+    private readonly categoryService: CategoryService,
+    private readonly expenseService: ExpenseService,
+    private readonly toastService: ToastService,
+    private readonly formBuilder: FormBuilder
+) {}
 
   addMonths = (number: number): void => {
     this.date = addMonths(this.date, number);
